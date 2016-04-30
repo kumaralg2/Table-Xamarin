bcc5381701ec10fd8ab05df06afe6618ec4c4ad4
Kiran <kumar.alg2@gmail.com>
2016-04-30 16:26:21 -0700

    TableView-Index

---
+++ tableView-Index/iOS/ViewController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+		
+using UIKit;
+using System.IO;
+using System.Collections.Generic;
+
+namespace tableViewIndex.iOS
+{
+	public partial class ViewController : UIViewController
+	{
+		UITableView table;
+
+		public ViewController (IntPtr handle) : base (handle)
+		{		
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			table = new UITableView (View.Bounds);
+			table.AutoresizingMask = UIViewAutoresizing.All;
+
+			var lines = File.ReadLines ("VegeData.txt");
+			List<string> veges = new List<string> ();
+			foreach(var l in lines){
+				veges.Add (l);
+			}
+			veges.Sort ((x,y) => {return x.CompareTo (y);});
+			string[] tableItems = veges.ToArray();
+
+
+			table.Source = new TableSource(tableItems,this);
+			Add (table);
+
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{		
+			base.DidReceiveMemoryWarning ();		
+			// Release any cached data, images, etc that aren't in use.		
+		}
+	}
+}
---
+++ tableView-Index/tableView-Index/MyClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace tableViewIndex
+{
+	public class MyClass
+	{
+		public MyClass()
+		{
+		}
+	}
+}
+
